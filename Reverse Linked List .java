4394b1ada8d3369a9ae8a9c51f8149e88e785e6a
Rafeek Biradar <90768585+biradarrafeek@users.noreply.github.com>
2024-02-16 01:07:13 +0530

    Create  Reverse Linked List .java

---
+++ Reverse Linked List .java
@@ -0,0 +1,46 @@
+/****************************************************************
+
+ Following is the class structure of the Node class:
+
+ class Node {
+     public int data;
+     public Node next;
+
+     Node()
+     {
+         this.data = 0;
+         this.next = null;
+     }
+     Node(int data)
+     {
+         this.data = data;
+         this.next = null;
+     }
+     Node(int data, Node next)
+     {
+         this.data = data;
+         this.next = next;
+     }
+ }
+
+ *****************************************************************/
+
+public class Solution
+{
+    public static Node reverseLinkedList(Node head)
+    {
+        // Write your code here.
+        Node prev = null;
+        Node curr = head;
+        Node next = null;
+
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+        return head;
+    }
+}
