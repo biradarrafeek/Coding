020129aa2bfb1e67479c9dd3eb29c128de17e5c6
Rafeek Biradar <90768585+biradarrafeek@users.noreply.github.com>
2024-06-02 22:22:58 +0530

    Create Member.java

---
+++ OOPS/02/Library Management System/Member.java
@@ -0,0 +1,42 @@
+package LibraryMangement;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Member {
+    private String id;
+    private String name;
+    private List<Book> borrowedBooks;
+
+    public Member(String id, String name) {
+        this.id = id;
+        this.name = name;
+        this.borrowedBooks = new ArrayList<>();
+    }
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Book> getBorrowedBooks() {
+		return borrowedBooks;
+	}
+
+	public void setBorrowedBooks(List<Book> borrowedBooks) {
+		this.borrowedBooks = borrowedBooks;
+	}
+
+    
+}
